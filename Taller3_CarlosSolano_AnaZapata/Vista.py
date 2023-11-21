c177662cd6571f1863e74df94879d4f63ae8d4bb
Carlos Solano <carlos.solanof@udea.edu.co>
2023-11-21 08:51:30 -0500

    Taller3_CarlosSolano_AnaZapata

---
+++ Taller3_CarlosSolano_AnaZapata/Vista.py
@@ -0,0 +1,78 @@
+
+# Importar librerias básicas
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIntValidator, QRegExpValidator
+from PyQt5.QtCore import Qt,QRegExp
+import sys
+
+class VentanaPrincipal(QMainWindow):
+    #constructor
+    def __init__(self, ppal=None):
+        super(VentanaPrincipal,self).__init__(ppal)
+        loadUi('VentanaLogin.ui',self)
+        self.setup()
+
+    #metodo para configurar las senales-slots y otros de la interfaz
+    def setup(self):
+        #se programa la senal para el boton
+        self.boton_ingresar.clicked.connect(self.accion_ingresar)
+
+
+    def asignarControlador(self,c):
+        self.__controlador = c
+
+    def accion_ingresar(self):
+        print("Boton presionado")
+        usuario = self.campo_usuario.text()
+        password = self.campo_password.text()
+
+        #esta informacion la debemos pasar al controlador
+        resultado = self.__controlador.validar_usuario(usuario,password)
+
+        msg = QMessageBox(self)
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle("Resultado")
+
+        #se selecciona el resultado de acuerdo al resultado de la operacion
+        if resultado == True:
+            self.abrirVista2()
+
+        else:
+            msg.setText("Usuario no Valido")
+            msg.show()
+            self.campo_usuario.clear()
+            self.campo_password.clear()
+        
+    def abrirVista2(self):
+        self.campo_usuario.clear()
+        self.campo_password.clear()
+        ventana_ingreso=Vista2(self)
+        self.hide()
+        ventana_ingreso.show()
+
+
+class Vista2(QDialog):
+    def __init__(self, ppal=None):
+        super().__init__(ppal)
+        loadUi("Vista2.ui",self)
+        self.__ventanaPadre = ppal
+        self.setup()
+
+    #metodo para configurar las senales-slots y otros de la interfaz
+    def setup(self):
+        #se programa la senal para el boton
+        self.BotonSalir.clicked.connect(self.accionSalir)
+
+    def asignarControlador(self,c):
+        self.__controlador = c
+
+    def accionSalir(self):
+        print("Boton presionado Salir")
+        self.hide()
+        self.__ventanaPadre.show()
+        
+
+
+        
+        
